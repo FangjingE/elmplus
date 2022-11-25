e9b27c75c445dcea4e887f09357c28f54cca5390
bacon1 <9623321+bacon1@user.noreply.gitee.com>
2022-11-25 15:51:00 +0000

    update elmclient/src/views/Bonuspoints.vue.
    Signed-off-by: bacon1 <9623321+bacon1@user.noreply.gitee.com>

--- elmclient/src/views/Bonuspoints.vue
+++ elmclient/src/views/Bonuspoints.vue
@@ -4,46 +4,23 @@
 	    <div class="card">
 	        <div class="card-body">
 	            <div class="row">
-	                <div class="col-6">积分余额：{{bonuspoints.balance}}</div>
+	                <div class="col-6">积分：0</div>
 	            </div>
 	        </div>
 	    </div>
-		<div class="checktranscation-button">
-			<button @click="checktranscation">查看流水</button>
-		</div>
 	</div>
 </template>
 
 <script>
 	export default {
 	name: "Bonuspoints",
-	data() {
-			return {
-	// 			userId: this.$route.query.userId,
-	 			bonuspoints: {}
-			}
-	 	},
-		created() {
-		         this.user = this.$getSessionStorage('user');
-				 this.$axios.post('BonuspointsController/getBonuspointsbyuserId', this.$qs.stringify({
-				 userid: this.user.userId,
-				 })).then(response => {
-				   this.bonuspoints = response.data;
-				   // this.$setSessionStorage('virtualWallet', virtualWallet);
-				  // console.log(this.VirtualWallet);
-				   }).catch(error => {
-				        console.error(error);
-				    });
-		},
-		methods:{
-			checktranscation(){
-				this.$router.push({
-					path: '/Bonuspointstranscation',
-				});
-			}
-		}
 	}
 </script>
 
-<style>
+<style scoped>
+.wrapper {
+    width: 100%;
+	height: 100%;
+	background-image: linear-gradient(to top, rgba(255, 0, 0, 0), rgb(59, 176, 246));
+}
 </style>
