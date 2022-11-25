df32e99d0d158b3c60d6c70853d0668dca499d18
bacon1 <9623321+bacon1@user.noreply.gitee.com>
2022-11-25 15:53:31 +0000

    update elmclient/src/views/Wallet.vue.
    Signed-off-by: bacon1 <9623321+bacon1@user.noreply.gitee.com>

--- elmclient/src/views/Wallet.vue
+++ elmclient/src/views/Wallet.vue
@@ -4,20 +4,23 @@
 			<p>我的钱包</p>
 		</Header>
         <div class="card">
-            <div class="card-body">
-                <div class="row">
-                    <div class="col-6">余额：{{virtualWallet.balance}}</div>
-                </div>
-            </div>
+
+           <button class="fa-list-ul" @click="checktranscation">查看流水</button>
+ 
         </div>
-		<div class="debit-button">
-			<button @click="todebit">提现</button>
-		</div>
-		<div class="credit-button">
-			<button @click="tocredit">充值</button>
-		</div>
-		<div class="credit-button">
-			<button @click="checktranscation">查看流水</button>
+		
+		<div class="box-below">
+			<div class="card-body">
+			    <div class="row">
+			        <div class="col-6">余额：{{virtualWallet.balance}}</div>
+			    </div>
+			</div>
+
+			<button class="charge" @click="todebit">提现</button>
+
+		    <button class="draw" @click="tocredit">充值</button>
+
+		    
 		</div>
     </div>
 </template>
@@ -71,17 +74,77 @@ export default {
 <style scoped>
 .wrapper {
     width: 100%;
+	height: 100%;
+	background-image: linear-gradient(to top, rgba(255, 0, 0, 0), rgb(59, 176, 246));
 }
 
 .card {
-    height: 10vh;
-    width: 92%;
-    margin-left: 4%;
-    margin-top: 15vh;
-    font-size: 4vw;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    
+    height: 9vw;
+    
+    font-size: 3.5vw;
+    color: white;
+    background-color: #359cff;
+    margin: 2vw 3vw 0 3vw;
+    
+    border-radius: 10px 10px 0 0;
 }
 
-.card div {
-    /* width: 45%; */
+.fa-list-ul {
+  background-color: #359cff;
+  border: #359cff;
 }
+
+.box-below {
+  display: flex;
+  flex-direction: column;
+  height: 35vw;
+  font-size: 3vw;
+  color: black;
+  background-color: #FFFFFF;
+  margin: 0 3vw 0 3vw;
+  border-radius: 10px;
+  border: solid 1px #DDD;
+  bottom: 5vw;
+}
+
+.card-body {
+	margin-left: 3vw;
+	flex: 1;
+	border-right: solid 1px #DDD;
+}
+
+.charge {
+  width: 15vw;
+  height: 6vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-left: 3vw;
+  margin-top: 27vw;
+  border-radius: 10px;
+  color: #ffffff;
+  background-color: #359cff;
+  position: absolute;
+  border: none;
+}
+
+.draw {
+  width: 15vw;
+  height: 6vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-left: 25vw;
+  margin-top: 27vw;
+  border-radius: 10px;
+  color: #ffffff;
+  background-color: #359cff;
+  position: absolute;
+  border: none;
+}
+
 </style>
