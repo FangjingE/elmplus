d517a88f5c77b3aa2b54535bbdb49fd863d1d04d
bacon1 <9623321+bacon1@user.noreply.gitee.com>
2022-11-26 13:21:16 +0000

    update elmclient/src/views/Mine.vue.
    Signed-off-by: bacon1 <9623321+bacon1@user.noreply.gitee.com>

--- elmclient/src/views/Mine.vue
+++ elmclient/src/views/Mine.vue
@@ -1,23 +1,16 @@
 <template>
 	<div class="wrapper">
-	    <Header name="我的"/>
-		<div>
-		<p>我的id: {{user.userId}}</p>
-		</div>
-	    <div class="card">
-	        <div class="card-body">
-	            <div class="row">
-					<router-link to="Wallet">我的钱包</router-link>
-	            </div>
-	        </div>
-	    </div>
+	    <Header>
+		    <p>个人主页</p>
+		</Header>
 		<div class="card">
-		    <div class="card-body">
-		        <div class="row">
-		           <router-link to="Bonuspoints">我的积分</router-link>
-		        </div>
-		    </div>
+		    <p>我的</p>
 		</div>
+		<div class="box-below">
+		    <p>我的id: {{user.userId}}</p>
+	        <router-link to="Wallet">我的钱包</router-link>
+	        <router-link to="Bonuspoints">我的积分</router-link>	
+		</div>			   
 	</div>
 </template>
 
@@ -47,19 +40,44 @@
 	background-image: linear-gradient(to top, rgba(255, 0, 0, 0), rgb(59, 176, 246));
 }
 
+.wrapper Header {
+  display: flex;
+  align-items: center;
+  height: 7vw;
+  font-weight: 550;
+  color: #FFFFFF;
+  padding-top: 3vw;
+}
+
 .card {
     display: flex;
     align-items: center;
-    justify-content: space-between;
-    
-    height: 9vw;
-    
+    justify-content: space-between;   
+    height: 9vw;   
     font-size: 3.5vw;
-    color: white;
+    color: #FFFFFF;
     background-color: #359cff;
-    margin: 2vw 3vw 0 3vw;
-    
+    margin: 2vw 3vw 0 3vw;   
     border-radius: 10px 10px 0 0;
+}
+
+.box-below {
+  display: flex;
+  flex-direction: column;
+  height: 35vw;
+  font-size: 3vw;
+  color: black;
+  background-color: #FFFFFF;
+  margin: 0 3vw 0 3vw;
+  border-radius: 10px;
+  border: solid 1px #DDD;
+  bottom: 5vw;
+}
+
+.card-body {
+	margin-left: 3vw;
+	flex: 1;
+	border-right: solid 1px #DDD;
 }
 
 </style>
