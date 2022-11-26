0dac7abfade121bcdcd964595a28a88d413f4593
bacon1 <9623321+bacon1@user.noreply.gitee.com>
2022-11-26 13:22:20 +0000

    update elmclient/src/views/Wallet.vue.
    Signed-off-by: bacon1 <9623321+bacon1@user.noreply.gitee.com>

--- elmclient/src/views/Wallet.vue
+++ elmclient/src/views/Wallet.vue
@@ -11,9 +11,7 @@
 		
 		<div class="box-below">
 			<div class="card-body">
-			    <div class="row">
-			        <div class="col-6">余额：{{virtualWallet.balance}}</div>
-			    </div>
+			    <div class="col-6">余额：{{virtualWallet.balance}}</div>
 			</div>
 
 			<button class="charge" @click="todebit">提现</button>
@@ -78,18 +76,24 @@ export default {
 	background-image: linear-gradient(to top, rgba(255, 0, 0, 0), rgb(59, 176, 246));
 }
 
+.wrapper Header {
+  display: flex;
+  align-items: center;
+  height: 7vw;
+  font-weight: 550;
+  color: #FFFFFF;
+  padding-top: 3vw;
+}
+
 .card {
     display: flex;
     align-items: center;
-    justify-content: space-between;
-    
-    height: 9vw;
-    
+    justify-content: space-between;   
+    height: 9vw;   
     font-size: 3.5vw;
-    color: white;
+    color: #FFFFFF;
     background-color: #359cff;
-    margin: 2vw 3vw 0 3vw;
-    
+    margin: 2vw 3vw 0 3vw;   
     border-radius: 10px 10px 0 0;
 }
 
