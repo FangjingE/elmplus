c939aff81baaced393dde159e26c1bf0006d96c4
FangjingE <614182859@qq.com>
2022-11-26 11:28:36 +0800

    修正页脚bug,merge

---
+++ elmclient/src/views/Bonuspointstranscation.vue
@@ -0,0 +1,45 @@
+<template>
+	<div class="wrapper">
+	<header>
+		<p>积分流水</p>
+	</header>
+	<ul class="transcation">
+		<li v-for="item in transcationArr" >
+			<div class="trancation-info">
+				<p>
+					{{item}}
+				</p>
+			</div>
+		</li>
+	</ul>
+	</div>
+</template>
+
+<script>
+	export default{
+		name:'Bonuspointstranscation',
+		data(){
+			return {
+			transcationArr:[],
+			}
+		},
+	created(){
+
+		this.user = this.$getSessionStorage('user');
+		
+		this.$axios.post('BonuspointsController/listtransactionbyid', this.$qs.stringify({
+			userid: this.user.userId
+		})).then(response => {
+			let result = response.data;
+			console.log(result);
+			this.transcationArr = result;
+			
+		}).catch(error => {
+			console.error(error);
+		});
+	}
+	}
+</script>
+
+<style>
+</style>
